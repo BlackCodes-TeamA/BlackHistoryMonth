908c067294b30ee392ae9bc568008483aa6fdf04
PrincessJaydeofWarri <etana@Astute.local>
2021-02-20 00:29:41 +0000

    front end first commit

---
+++ Documents/Jayde/Hackathon/bhm-hackathon/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='login-form'>
+      <form className='form'>
+      <h1>BHM Hackathon
+      </h1>
+      <div className='form-inputs'>
+        <label htmlFor='username'
+        className='form-label'>
+        Username
+        </label>
+        <input
+        id='username'
+        type='text' name='username'
+        className='form-input'
+        placeholder="Username"/>
+        </div>
+          <div className='form-inputs'>
+            <label htmlFor='Password'
+            className='form-label'>
+            Password
+            </label>
+            <input
+            id='password'
+            type='password' name='password'
+            className='form-input'
+            placeholder="Password"/>
+            </div>
+              <button className='form-input-btn' type='submit'>
+              Login 
+              </button>
+      </form>
+    </div>
+
+  );
+}
+
+export default App;
